6a92c628915e39cccd936968ca06a379049fd560
Eli Sacks <bastlifa@gmail.com>
2020-01-20 22:35:23 -0500

    add vertex, add edge, get neighbors

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,19 +13,25 @@ class Graph:
         """
         Add a vertex to the graph.
         """
+        self.vertices[vertex_id] = None
         pass  # TODO
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
+        if self.vertices[v1]:
+            self.vertices[v1].add(v2)
+        else:
+            self.vertices[v1] = {v2}
         pass  # TODO
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
+        # pass  # TODO
 
     def bft(self, starting_vertex):
         """
@@ -118,7 +124,7 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
-    graph.bft(1)
+    # graph.bft(1)
 
     '''
     Valid DFT paths:
@@ -127,19 +133,19 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
-    graph.dft(1)
-    graph.dft_recursive(1)
+    # graph.dft(1)
+    # graph.dft_recursive(1)
 
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    print(graph.bfs(1, 6))
+    # print(graph.bfs(1, 6))
 
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
-    print(graph.dfs(1, 6))
-    print(graph.dfs_recursive(1, 6))
+    # print(graph.dfs(1, 6))
+    # print(graph.dfs_recursive(1, 6))
